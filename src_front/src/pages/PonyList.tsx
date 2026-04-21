43d7a4c8e3b3fe1c3ffa64736fdd35530f94810f
Charles Ross <chivalry@mac.com>
2026-04-21 15:54:30 -0700

    Update spacing.

--- src_front/src/pages/PonyList.tsx
+++ src_front/src/pages/PonyList.tsx
@@ -6,32 +6,32 @@ import { listPonies, deletePony, type Pony } from '../api/ponies'
 import { PonyCard } from '../components/PonyCard'
 
 export default function PonyList() {
-  const [ponies, setPonies] = useState<Pony[]>([])
+    const [ponies, setPonies] = useState<Pony[]>([])
 
-  useEffect(() => {
-    listPonies().then((r) => setPonies(r.data))
-  }, [])
+    useEffect(() => {
+        listPonies().then((r) => setPonies(r.data))
+    }, [])
 
-  const handleDelete = async (id: number) => {
-    await deletePony(id)
-    setPonies((prev) => prev.filter((p) => p.id !== id))
-  }
+    const handleDelete = async (id: number) => {
+        await deletePony(id)
+        setPonies((prev) => prev.filter((p) => p.id !== id))
+    }
 
-  return (
-    <Box>
-      <Box sx={{ display: 'flex', justifyContent: 'space-between', mb: 2 }}>
-        <Typography variant="h4">Ponies</Typography>
-        <Button variant="contained" component={Link} to="/ponies/new">
-          Add Pony
-        </Button>
-      </Box>
-      <Grid container spacing={2}>
-        {ponies.map((pony) => (
-          <Grid item xs={12} sm={6} md={4} key={pony.id}>
-            <PonyCard pony={pony} onDelete={handleDelete} />
-          </Grid>
-        ))}
-      </Grid>
-    </Box>
-  )
+    return (
+        <Box>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', mb: 2 }}>
+                <Typography variant="h4">Ponies</Typography>
+                <Button variant="contained" component={Link} to="/ponies/new">
+                    Add Pony
+                </Button>
+            </Box>
+            <Grid container spacing={2}>
+                {ponies.map((pony) => (
+                    <Grid item xs={12} sm={6} md={4} key={pony.id}>
+                        <PonyCard pony={pony} onDelete={handleDelete} />
+                    </Grid>
+                ))}
+            </Grid>
+        </Box>
+    )
 }
