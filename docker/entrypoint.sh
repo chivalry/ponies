7075eb2b5e4e510e46f44a65ff46bf0297a115ba
Charles Ross <chivalry@mac.com>
2026-04-26 15:36:08 -0700

    Wrap flask in waiting loop

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+
 set -e
-flask db upgrade
+until flask db upgrade; do
+  echo "Database not ready, retrying in 2s..."
+  sleep 2
+done
 python -m src_back.seed
 gunicorn --bind 0.0.0.0:5000 --reload src_back.app:app
